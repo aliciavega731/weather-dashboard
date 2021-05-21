670fcecfad43a598314d62515f55037386848f2f
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-05-21 09:50:26 -0700

    added elements and fixed html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-// Need to add event listeners
+// Initial the page by selecting things from doc
+
+function init(){
+  var inputElement = document.getElementById("input-city");
+  var searchElement = document.getElementById("button-search");
+  var clearElement = document.getElementById("clear-history");
+}
