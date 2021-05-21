b0707ea2b5978b4c1842ea565a9a2507f32c3e7d
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-05-21 10:23:17 -0700

    updated HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,25 @@
 // Initial the page by selecting things from doc
+function init() {
+  var inputEl = document.getElementById("input-city");
+  var searchEl = document.getElementById("button-search");
+  var clearEl = document.getElementById("clear-history");
+}
 
-function init(){
-  var inputElement = document.getElementById("input-city");
-  var searchElement = document.getElementById("button-search");
-  var clearElement = document.getElementById("clear-history");
+// Pasting API key here
+var APIKey = "4d63f1f3bfd4180e83c77404a79e20da";
+
+// Need to create cityName to reference later
+function collectWeather(cityName) {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=" +
+    APIKey;
+  axios.get(queryURL).then(function (data) {
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth();
+    const year = currentDate.getYear();
+    nameEl.innerHTML =
+      response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+  });
 }
